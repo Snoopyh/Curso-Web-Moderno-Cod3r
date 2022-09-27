090d887ea1ec591633468e64f184f17f766d298e
Snoopyh <ismaelxd500@gmail.com>
2022-09-27 10:43:28 -0300

    funcoes

---
+++ exercicios in JS/Funçoes/tiposDeclaracoes.js
@@ -0,0 +1,12 @@
+// function declaration
+function nomeFunção(parametros){
+  return corpoDaFunção
+}
+// function expression
+const nomeDaConst = function nomeFunção(parametros){
+  return corpoDaFunção
+}
+// function named function expression (POUCA USADA)
+const mult = function mult(parametro1,parametro2){
+  return parametro1 * parametro2
+}
