69af44b27ac033477cd60a9d2caa23b62eecca30
Snoopyh <ismaelxd500@gmail.com>
2022-10-21 14:44:24 -0300

    Exercicio manipulçao array

--- exercicios in JS/ExerciciosFundamentos/exercicios30.js
+++ exercicios in JS/ExerciciosFundamentos/exercicios30.js
@@ -2,13 +2,25 @@
 As notas de cada estudante estarão num array, conforme exemplo abaixo. 
 Você deverá calcular a média da nota de cada aluno e retornar um objeto com
 os atributos nome e media, que indica o aluno que teve o melhor desempenho nas notas.*/
-function recerberMelhorEstudante(obj){
-  
-}
 
-recerberMelhorEstudante({
-  Joao: [8, 7.6, 8.9, 6], // média 7.625
-  Mariana: [9, 6.6, 7.9, 8], // média 7.875
-  Carla: [7, 7, 8, 9] // média 7.75
-  }) // retornará { nome: "Mariana", media: 7.875 }
+const soma = array => array.reduce((acumulador , atual) => acumulador + atual ,0)
+const media = array => soma(array) / array.length
+
+function recerberMelhorEstudante(estudantes){
+  const estudantesComMedias = Object.entries(estudantes).map( estudantes => {
+    const chave = 0,
+          valor = 1
+    return { nome: estudantes [chave], media: media(estudantes[valor]) }
+  })
+  const estudantesOrdenados = estudantesComMedias.sort((estudanteA , estudanteB) =>
+  estudanteB.media - estudanteA.media)
+  const melhoresEstudantes = estudantesOrdenados[0]
+  return melhoresEstudantes
+}
+  
+console.log(recerberMelhorEstudante({
+  Joao: [8, 7.6, 8.9, 6], 
+  Mariana: [9, 6.6, 7.9, 8], 
+  Carla: [7, 7, 8, 9] 
+  }) )
   
