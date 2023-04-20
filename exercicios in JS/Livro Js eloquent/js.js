1376a25b839fd0c8d5e151617cde6f4500688d72
Snoopyh <ismaelxd500@gmail.com>
2023-04-20 12:19:04 -0300

    Last changes

---
+++ exercicios in JS/Livro Js eloquent/js.js
@@ -0,0 +1,12 @@
+// String
+console.log("A newline character is written like “\n”.")
+console.log("A newline character is written like \"\\n\".")
+
+//
+console.log("con" + "cat" + "e" + "nate")
+
+// operadores Unarios
+console.log(typeof 4.5)
+// → number
+console.log(typeof "x")
+// → string
