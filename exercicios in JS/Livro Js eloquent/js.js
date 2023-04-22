b649e896d1e26cf338a1b681fcf1e16dc63cc1fe
Snoopyh <ismaelxd500@gmail.com>
2023-04-22 15:21:45 -0300

    Last commit

--- exercicios in JS/Livro Js eloquent/js.js
+++ exercicios in JS/Livro Js eloquent/js.js
@@ -10,3 +10,27 @@ console.log(typeof 4.5)
 // → number
 console.log(typeof "x")
 // → string
+
+//chapter 2 
+var debitoDoBozzo = 700
+debitoDoBozzo = debitoDoBozzo - 200
+console.log(debitoDoBozzo)
+
+// var theNumber = Number(prompt("Pick a number" , ""));
+// if (!isNaN(theNumber))
+//   console.log("Your Number is the square root of" +
+//   theNumber + theNumber)
+
+// var number = 0;
+// while(number <= 12){
+//   console.log(number);
+//   number = number+ 2;
+// }
+
+var result = 1;
+var count = 0;
+while(count < 10){
+  result = result * 2
+  count = count + 1;
+} 
+console.log(result)
